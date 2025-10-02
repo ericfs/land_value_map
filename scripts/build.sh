9cea80d4873d3305861d1f8a1f062c7627b6f80f
ericfs <851850+ericfs@users.noreply.github.com>
2025-10-02 15:17:14 -0400

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Expects to be run from the project root directory.
+
+build_dir=`pwd`
+
+# Assumes there is a virtual environment
+python_interpreter="`pwd`/.venv/bin/python3"
+
+ansible_variables="ansible_python_interpreter=\"${python_interpreter}\" build_dir=\"${build_dir}\""
+
+ansible-playbook ansible/main.yml -e "${ansible_variables}"
